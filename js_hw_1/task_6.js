6698d62c7c141651685c15a7b400d4587368b4ed
Mariia Syniushkina <marysinyushkina@gmail.com>
2022-12-23 17:05:20 +0100

    added check for spaces in prompt

--- js_hw_1/task_6.js
+++ js_hw_1/task_6.js
@@ -1,8 +1,18 @@
 let input = prompt("Enter Number");
 let total = 0;
 
-while (typeof input == "string") {
-  if (!isNaN(Number(input))) {
+// while (typeof input == "string") {
+//   if (!isNaN(Number(input))) {
+//     total += Number(input);
+//   } else {
+//     alert("Not a number entered, try again");
+//   }
+//   input = prompt("Enter Number");
+// }
+
+// added check if entered number contains spaces or is string contains only spaces
+for (input; typeof input == "string"; input) {
+  if (!isNaN(Number(input)) && input.trim().length !== 0) {
     total += Number(input);
   } else {
     alert("Not a number entered, try again");
